79ee2965b732f8e01e6b7f028f1bb3bf3a32a7a3
Afonso Fernández Nogueira <fonzzo@gmail.com>
2013-01-09 13:14:25 +0100

    commit inicial

---
+++ django_localflavor_es_ga/es_ga_provinces.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+from __future__ import unicode_literals
+
+
+PROVINCE_CHOICES = (
+    ('15', _(u'A Coruña')),
+    ('27', _(u'Lugo')),
+    ('32', _(u'Ourense')),
+    ('36', _(u'Pontevedra')),
+)
